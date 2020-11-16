65de375b72a71ad50646dc3fa9271bc0b47ca2da
KienbMi <m_kienberger@gmx.net>
2020-11-16 15:13:40 +0100

    Web select member

--- ClubAdministration.Web/Pages/Index.cshtml.cs
+++ ClubAdministration.Web/Pages/Index.cshtml.cs
@@ -1,26 +1,64 @@
 ﻿using ClubAdministration.Core.Contracts;
+using ClubAdministration.Core.DataTransferObjects;
+using ClubAdministration.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ClubAdministration.Web.Pages
 {
-  public class IndexModel : PageModel
-  {
-    private readonly IUnitOfWork _unitOfWork;
-
-    public IndexModel(IUnitOfWork unitOfWork)
+    public class IndexModel : PageModel
     {
-      _unitOfWork = unitOfWork;
-    }
+        private readonly IUnitOfWork _unitOfWork;
 
-    public IActionResult OnGet()
-    {
-      return Page();
-    }
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
 
-    public IActionResult OnPost()
-    {
-      return Page();
+        [BindProperty]
+        public MemberDto[] Members { get; set; }
+        [BindProperty]
+        public Section[] Sections { get; set; }
+        [BindProperty]
+        public int SelectedSectionId { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            Sections = (await _unitOfWork
+                .SectionRepository
+                .GetAllSectionsAsync())
+                .ToArray();                            
+
+            Members = new MemberDto[] { };
+            if (Members == null)
+            {
+                return NotFound();
+            }
+            
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            Sections = (await _unitOfWork
+                .SectionRepository
+                .GetAllSectionsAsync())
+                .ToArray();
+
+            Members = (await _unitOfWork
+                .MemberRepository
+                .GetMemberDtoBySectionIdAsync(SelectedSectionId))
+                .ToArray();
+
+            if (Members == null)
+            {
+                Members = new MemberDto[] { };
+                return NotFound();
+            }
+            
+            return Page();
+        }
     }
-  }
 }
